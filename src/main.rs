6c66ab174eefb20438d437a11f95ffd0f7ea9ad0
mirzaipatrik <patrik.mirzai@gmail.com>
2024-08-11 14:19:09 +0300

    feat: add datoCMS support

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,24 @@
 mod utils; // Declare the `utils` module
-
+use serde_json::json;
 use std::error::Error;
-
-use utils::embedding_utils::get_embedding;
 use utils::db_connect::db::upsert_data;
 use utils::db_connect::db::DbProperties;
+use utils::embedding_utils::get_embedding;
 use utils::get_datocms_stories::get_stories;
+use utils::get_datocms_stories::StoriesResponse; // Import serde_json and Value.
 
 const QUERY: &str = r#"
-query {
-  allStories(
-    locale: en
-    filter: {date: {gt: "2018-01-01", lt: "2022-01-01"}}
-    orderBy: date_DESC
-  ) {
+query Home($language: SiteLocale, $skip: IntType, $yearStart: Date, $yearEnd: Date) {
+  allStories(locale: $language, first: 20, skip: $skip, filter: {date: {gt: $yearStart, lt: $yearEnd}}, orderBy: date_DESC) {
     storyNumber
     date
+    description
+    storyContent {
+      __typename
+      ... on ParagraphRecord {
+        paragraphText
+      }
+    }
   }
 }
 "#;
@@ -31,7 +34,13 @@ fn main() {
     let response = upsert_data(props);
     println!("{:#?}", response);
 
-    let datocms: Result<String, Box<dyn Error>> = get_stories(QUERY);
-    println!("{:#?}", datocms);
+    let variables = json!({
+        "language": "en",
+        "skip": 0,
+        "yearStart": "2018-01-01",
+        "yearEnd": "2022-01-01"
+    });
 
+    let datocms: Result<StoriesResponse, Box<dyn Error>> = get_stories(QUERY, variables);
+    println!("{:#?}", datocms);
 }
--- src/utils/get_datocms_stories.rs
+++ src/utils/get_datocms_stories.rs
@@ -1,11 +1,48 @@
 use dotenv::dotenv;
 use reqwest::blocking::Client;
-use reqwest::header::{HeaderMap, AUTHORIZATION, ACCEPT, CONTENT_TYPE};
-use serde_json::json;
+use reqwest::header::{HeaderMap, ACCEPT, AUTHORIZATION, CONTENT_TYPE};
+use serde_json::{json, Value}; // Import serde_json and Value.
 use std::env;
 use std::error::Error;
 
-pub fn get_stories(query: &str) -> Result<String, Box<dyn Error>> {
+use serde::{Deserialize, Serialize};
+
+// Represents the `storyContent` field in a Story.
+#[derive(Debug, Serialize, Deserialize)]
+#[serde(rename_all = "camelCase")]
+pub struct StoryContent {
+    #[serde(rename = "__typename")]
+    typename: String,
+    #[serde(rename = "paragraphText")]
+    paragraph_text: Option<String>,
+}
+
+// Represents a story item.
+#[derive(Debug, Serialize, Deserialize)]
+pub struct Story {
+    date: String,
+    #[serde(rename = "storyNumber")]
+    story_number: u32, // Updated to snake_case
+    description: String,
+    #[serde(rename = "storyContent")]
+    story_content: Vec<StoryContent>, // Updated to snake_case
+}
+
+// Represents the `allStories` field in the GraphQL response.
+#[derive(Debug, Serialize, Deserialize)]
+pub struct AllStories {
+    #[serde(rename = "allStories")]
+    all_stories: Vec<Story>, // Updated to snake_case
+}
+
+// Represents the top-level response object containing `data`.
+#[derive(Debug, Serialize, Deserialize)]
+pub struct StoriesResponse {
+    data: AllStories,
+}
+
+pub fn get_stories(query: &str, variables: Value) -> Result<StoriesResponse, Box<dyn Error>> {
+    // Return a Value instead of String.
     dotenv().ok();
 
     let dato_cms_token = env::var("DATOCMS_API_TOKEN")?;
@@ -18,7 +55,9 @@ pub fn get_stories(query: &str) -> Result<String, Box<dyn Error>> {
     headers.insert(ACCEPT, "application/json".parse()?);
 
     let query_body = json!({
-        "query": query
+        "query": query,
+        "variables": variables
+
     });
 
     let response = http_client
@@ -27,6 +66,9 @@ pub fn get_stories(query: &str) -> Result<String, Box<dyn Error>> {
         .body(query_body.to_string())
         .send()?;
 
-    let response_text = response.text()?;
-    Ok(response_text)
+    let response_text: String = response.text()?; // Get the response as a string.
+
+    // Deserialize the string into a serde_json::Value
+    let json_response: StoriesResponse = serde_json::from_str(&response_text)?;
+    Ok(json_response) // Return the JSON object.
 }
